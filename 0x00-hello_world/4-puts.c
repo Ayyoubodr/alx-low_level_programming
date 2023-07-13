eb97872ecedddfb169d0dde89fb09838d443cea8
Ayyoub <you@example.com>
2023-07-13 16:54:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a specific sentence and returns 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
