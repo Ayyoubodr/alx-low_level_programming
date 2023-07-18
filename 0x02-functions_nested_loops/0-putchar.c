b652c59eeefe8869250f4d0671b4a57a3f795db0
Ayyoub <you@example.com>
2023-07-18 07:13:05 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'Prints the alphabet in lowercase and uppercase'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+write(1, "_putchar\n", 9);
+return (0);
+}
