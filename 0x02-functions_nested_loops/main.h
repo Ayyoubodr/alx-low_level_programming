4c4d9654690252cef61335d810d84e02b5e6bd0a
Ayyoub <you@example.com>
2023-07-18 17:35:42 -0700

    Learn your times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,9 +21,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 void jack_bauer(void);
-void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-
+void times_table(void);
 #endif
