28289b2d1ca9b9f28870fab73301d27c583f91f5
Ayyoub <you@example.com>
2023-08-16 04:04:52 -0700

    one

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/*by div-styl*/
+
+/**
+ * print_name -print the name
+ * @name: the name
+ * @f: the fun pointer
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
