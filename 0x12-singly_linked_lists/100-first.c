5d613b86348f8a72c19c6123d6452dd7bdde4629
Ayyoub <you@example.com>
2023-08-24 06:50:27 -0700

    one

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void print_into_theconsol(void)__attribute__((constructor));
+/* by div-styl*/
+/**
+ * print_into_theconsol - prints a string into the consol
+ * Return: void
+*/
+void print_into_theconsol(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
